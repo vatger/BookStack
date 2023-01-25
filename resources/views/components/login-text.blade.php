40c91fdf20394730669017e76dd7afbc4e49c862
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-01-25 23:58:24 +0100

    login

--- resources/views/components/login-text.blade.php
+++ resources/views/components/login-text.blade.php
@@ -1,7 +1,5 @@
 <div>
     <p>
-        Bei Anmeldung stimmst Du automatisch unseren Nutzungsbedingungen zu.
-        Diese findest Du
-        <a src="vatsim-germany.org">hier</a>.
+        Ich bin einverstanden damit, dass ich beim Erstellen von Inhalten in der VATSIM Germany Knowledgebase, VATSIM Germany ein nicht ausschließliches unentgeltliches, zeitlich, örtlich und inhaltlich unbeschränktes, übertragbares, unterlizenzierbares und unwiderrufliches Nutzungsrecht an den von mir erstellten Inhalten einräume. Ich garantiere, dass an den von mir erstellten Inhalten keine Rechte Dritter bestehen und durch die eingeräumten Nutzungsrechte keine Rechte Dritter verletzt werden.
     </p>
 </div>
